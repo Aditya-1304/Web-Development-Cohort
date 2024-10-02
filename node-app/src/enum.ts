59284e7d9ac068da40e76d59e569768181d9375b
Aditya Mandal <mandaladitya1614@gmail.com>
2024-10-02 12:50:33 +0530

    Final Week-9 Commit

---
+++ node-app/src/enum.ts
@@ -0,0 +1,16 @@
+enum Directions {
+  Up = 'up',
+  Down = 'down',
+  Left = 'left',
+  Right = 'right',  
+}
+function doSomething(keyPressed : Directions) {
+  if (keyPressed == Directions.Up){
+
+  }
+}
+
+doSomething(Directions.Left);
+doSomething(Directions.Right);
+console.log(Directions.Up);
+console.log(Directions.Down);
