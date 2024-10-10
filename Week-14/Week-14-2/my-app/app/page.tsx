f5801353554c3fb45d599e82c1c6f9860d4e9dc8
Aditya Mandal <mandaladitya1614@gmail.com>
2024-10-10 23:08:17 +0530

    Week-14-2 final

--- Week-14/Week-14-2/my-app/app/page.tsx
+++ Week-14/Week-14-2/my-app/app/page.tsx
@@ -1,9 +1,29 @@
-import axios from "axios";
+//INSTEAD OF THIS 
 
+// import axios from "axios";
+
+// async function getUserData() {
+//   const response = await axios.get(" https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+//   return response.data;
+// }
+
+// WE CAN DO : 
+// import { PrismaClient } from "@prisma/client";
+
+// const client = new PrismaClient();
+import client from "@/db"
 async function getUserData() {
-  const response = await axios.get(" https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
-  return response.data;
+  try {
+    const user = await client.user.findFirst({});
+	  return {
+      name: user?.username,
+      email: user?.username
+    }
+  }  catch(e) {
+    console.log(e);
+  }
 }
+
 export default async function Home() {
   const userDetail = await getUserData();
   return (
