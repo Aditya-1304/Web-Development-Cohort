fe08f85463f34cfe13aefc188203fd920aa051ea
Aditya Mandal <mandaladitya1614@gmail.com>
2024-10-12 23:02:53 +0530

    Fixing errors

--- Week-7/week-7-2/src/App.jsx
+++ Week-7/week-7-2/src/App.jsx
@@ -1,50 +1,49 @@
-import { useContext } from "react"
-import { CountContext } from "./context";
-import { RecoilRoot, useRecoilState, useRecoilValue} from "recoil";
+
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
 import { countAtom, evenSelector } from "./store/atoms/count";
 
 function App() {
 
-return (
-  <div>
-    <RecoilRoot>
-       <Count />
-    </RecoilRoot>      
-  </div>
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
   )
 }
 
-function Count(){
-    return <div>
-        <CountRerender />
-        <Buttons  />
-        <IsEven/>
-    </div>
+function Count() {
+  return <div>
+    <CountRerender />
+    <Buttons />
+    <IsEven />
+  </div>
 }
 
-function IsEven(){
+function IsEven() {
   const count = useRecoilValue(evenSelector);
-  if(count % 2 == 0){
+  if (count % 2 == 0) {
     return <div>It is Even</div>
-  }else
-  return <div>It is Odd</div>
+  } else
+    return <div>It is Odd</div>
 }
 
 function CountRerender() {
-    const count = useRecoilValue(countAtom);
-    return <div>
-        {count}
-    </div>
+  const count = useRecoilValue(countAtom);
+  return <div>
+    {count}
+  </div>
 }
-function Buttons(){
-    const [count,setCount] = useRecoilState(countAtom); 
-    return <div>
-        <button onClick={() => {
-            setCount(count+1)
-        }}>Increase</button>
-        <button onClick={() => {
-            setCount(count - 1)
-        }}>Decrease</button>
-    </div>
+function Buttons() {
+  const [count, setCount] = useRecoilState(countAtom);
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>Increase</button>
+    <button onClick={() => {
+      setCount(count - 1)
+    }}>Decrease</button>
+  </div>
 }
 export default App
