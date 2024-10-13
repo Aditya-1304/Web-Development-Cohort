e59cfced1bcfa10813619ff6d397fb0b5d2cc38e
Aditya Mandal <mandaladitya1614@gmail.com>
2024-10-13 23:18:50 +0530

    i

--- Week-12/Week-12-4/src/index.ts
+++ Week-12/Week-12-4/src/index.ts
@@ -1,11 +1,12 @@
 // write a function to create a users table in your database.
 import { Client } from 'pg'
  
-//connection to the d
+//connection to the db
 const client = new Client({
   connectionString: process.env.POSTGRES_URI
 })
 
+//main function which uses the db connection to take user values
 async function insertUserData(username : string , email : string , password : string) {
     await client.connect()
     const result = await client.query(`
