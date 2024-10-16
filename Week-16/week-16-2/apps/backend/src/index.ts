0abada9636b5e5b5f5c180e3a958a73194b3e5bd
Aditya Mandal <mandaladitya1614@gmail.com>
2024-10-16 14:42:26 +0530

    Final Week-16 commit

--- Week-16/week-16-2/apps/backend/src/index.ts
+++ Week-16/week-16-2/apps/backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import {BACKEND_URL} from "@repo/common/config"
+
 
 const app = express();
 
+console.log(BACKEND_URL);
 app.get("/", (req, res) => {
   res.json({
     message : "Hello World"
