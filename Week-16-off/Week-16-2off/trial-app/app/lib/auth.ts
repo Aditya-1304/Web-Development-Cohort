feddac88015de0c8d125c246c2ffeb93c7b8a3bf
Aditya Mandal <mandaladitya1614@gmail.com>
2024-10-20 19:40:26 +0530

    Week-17 init

--- Week-16-off/Week-16-2off/trial-app/app/lib/auth.ts
+++ Week-16-off/Week-16-2off/trial-app/app/lib/auth.ts
@@ -1,4 +1,8 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import { pages } from "next/dist/build/templates/app-page";
+
 
 export const NEXT_AUTH = {
   providers : [
@@ -17,6 +21,14 @@ export const NEXT_AUTH = {
           email : "johndoe@gmail.com"
         }
       }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || ""
     })
   ],
   secret : process.env.NEXTAUTH_SECRET,
@@ -41,5 +53,9 @@ export const NEXT_AUTH = {
       }
       return session;
     }
+  },
+  pages :{
+    signIn : "/signin",
+    
   }
 }
